e350c7e7ef5f33e88710803c20799b89867490f1
Dani Pletter <dpletter@users.noreply.github.com>
2024-08-22 14:37:39 -0700

    switch from yarn to npm

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,8 +43,8 @@ EOF
 # - Build react bundle
 # - Sync using our dedicated profile and suppress verbose messages.
 #   All other flags are optional via the `args:` directive.
-sh -c "yarn" \
-&& sh -c "${NODE_ENV_PREPEND} yarn build" \
+sh -c "npm" \
+&& sh -c "${NODE_ENV_PREPEND} npm build" \
 && sh -c "aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile react-deploy-to-s3-action \
               --no-progress \

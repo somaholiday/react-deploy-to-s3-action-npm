49a810b79d4bb992fe809d1a6f30c052d6b5e8b4
Dani Pletter <dpletter@users.noreply.github.com>
2024-08-22 14:45:43 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,7 +43,7 @@ EOF
 # - Build react bundle
 # - Sync using our dedicated profile and suppress verbose messages.
 #   All other flags are optional via the `args:` directive.
-sh -c "npm" \
+sh -c "npm --version" \
 && sh -c "${NODE_ENV_PREPEND} npm run build" \
 && sh -c "aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile react-deploy-to-s3-action \
